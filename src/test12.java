a2b2267acdafa27c718f4cd3863323104016426b
Aswin_S_Aswin <rtraswinppgit@gmail.com>
2025-12-23 00:09:35 +0530

    read

--- src/test12.java
+++ src/test12.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class test12 {
+    public static void main(String[] args){
+        int income = 120_000;
+        boolean check = (income <= 100_000);
+        System.out.println(check);
+        String high = 100_000 < income ? "So it's fine" : "Not ok Bro";
+        System.out.println(high);
+
+    }
 }

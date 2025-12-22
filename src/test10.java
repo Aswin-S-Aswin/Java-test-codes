12d1c08d46cb2fd3546272cece1a4e13238e11b2
Aswin_S_Aswin <rtraswinppgit@gmail.com>
2025-12-22 23:57:50 +0530

    Hlo

---
+++ src/test10.java
@@ -0,0 +1,17 @@
+//Comaparision Operator
+
+public class test10 {
+    public static void main(String[] args){
+
+        int x = 1;
+        int y = 1;
+        System.out.println(x == y);
+//Logical Operator
+        boolean a = true;
+        boolean b = false ;
+        boolean c = a || b;
+        System.out.println(c);
+        System.out.println(!c);
+
+    }
+}
---
+++ src/test11.java
@@ -0,0 +1,13 @@
+public class test11 {
+    public static void main(String[] args){
+        int temp = 32;
+        if (temp ==32){
+            System.out.println("Ennaaa ddaaaa");
+        } else if (temp < 35) {
+            System.out.println("Parru bro");
+
+        }
+
+
+    }
+}

5d5874482e62340f9d75536ebcecf1e2beed7f3e
Aswin_S_Aswin <rtraswinppgit@gmail.com>
2025-12-25 20:25:29 +0530

    last update

--- src/test13.java
+++ src/test13.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class test13 {
     public static void main(String[] args){
+        for (int x = 1; x <= 5; x++) {
+            System.out.println(x +". Hello guys");
+        }
 
+        String input = "";
+        Scanner scn = new Scanner(System.in);
+        while (!input.equals("quit")){
+            System.out.print("Input : ");
+            input =scn.next().toLowerCase();
+            System.out.println(input);
+        }
     }
 }
---
+++ src/test14.java
@@ -0,0 +1,9 @@
+public class test14 {
+    public static void main(String[] args){
+        int a = 0;
+        for (int days = 1; days <= 5; days++){
+            a = a + days;
+            System.out.println(a);
+        }
+    }
+}
